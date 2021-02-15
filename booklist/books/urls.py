4ec5482d998a02acd8e02573b967cb5817c57771
Jasmin Alian <jasmin.alian@student.careeria.fi>
2021-02-15 20:07:34 +0200

    Created form, urls and views for functions(update, create, delete).

--- booklist/books/urls.py
+++ booklist/books/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"), 
+    path("create/", views.CreateBookForm.as_view(), name="add_book"),
+    path("update/<pk>/", views.UpdateBookForm.as_view(), name="update_book"),
+    path("delete/<pk>/", views.DeleteBookForm.as_view(), name="delete_book"),
     ]
--- booklist/books/views.py
+++ booklist/books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 def index(request):
     book_list = Book.objects.order_by("title")
@@ -7,3 +10,21 @@ def index(request):
         "book_list": book_list,
     }
     return render(request, "index.html", context)
+
+class CreateBookForm(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = "create.html"
+    success_url = reverse_lazy("index")
+
+class UpdateBookForm(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = "update.html"
+    success_url = reverse_lazy("index")
+
+class DeleteBookForm(DeleteView):
+    model = Book
+    form_class = BookForm
+    template_name = "delete.html"
+    success_url = reverse_lazy("index")
